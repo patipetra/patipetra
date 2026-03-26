211855c4bcf3e4960191e7282f873aa6fd94000e
patipetra <patipetraa1@gmail.com>
2026-03-26 22:20:05 +0000

    fix: COOP header for Firebase Google auth

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = nextConfig
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
+    ],
+  },
+};
+
+module.exports = nextConfig;
